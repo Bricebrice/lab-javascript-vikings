30f7647ead3b3a789a9b788ec0f89f88fac20832
Brice Cabioch <cabioch.brice@gmail.com>
2023-10-17 21:57:24 +0200

    iteration 2 completed

--- src/viking.js
+++ src/viking.js
@@ -13,7 +13,26 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
+const viking1 = new Viking("Brice", 100, 50);
+const damageMessage = viking1.receiveDamage(20);
+console.log(damageMessage);
 
 // Saxon
 class Saxon {}
