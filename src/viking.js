426af8a5b9970b6d1676dda09458770ad27c1066
Brice Cabioch <cabioch.brice@gmail.com>
2023-10-20 22:23:40 +0200

    bonus iteration 4 completed

--- src/viking.js
+++ src/viking.js
@@ -19,6 +19,7 @@ class Viking extends Soldier {
     this.name = name;
   }
   receiveDamage(damage) {
+    // maybe possible to use super ?
     this.health -= damage;
     if (this.health > 0) {
       return `${this.name} has received ${damage} points of damage`;
@@ -36,9 +37,9 @@ class Viking extends Soldier {
 
 // Saxon
 class Saxon extends Soldier {
-  attack(){
+  attack() {
     return this.strength;
-  };
+  }
   receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
@@ -47,8 +48,56 @@ class Saxon extends Soldier {
       return `A Saxon has died in combat`;
     }
   }
-
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(Viking) {
+    this.vikingArmy = [Viking];
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy = [Saxon];
+  }
+  vikingAttack() {
+    // random index from the array
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    // random saxon from the array based on the (random) index
+    const saxon = this.saxonArmy[randomSaxon];
+    const viking = this.vikingArmy[randomViking];
+    // saxon receive damage
+    const result = saxon.receiveDamage(viking.strength);
+    // remove dead saxon
+    if (saxon.health <= 0) {
+      this.saxonArmy.splice(0, 1);
+    }
+    return result;
+  }
+  saxonAttack() {
+    // random index from the array
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    // random saxon from the array based on the (random) index
+    const saxon = this.saxonArmy[randomSaxon];
+    const viking = this.vikingArmy[randomViking];
+    // saxon receive damage
+    const result = viking.receiveDamage(saxon.strength);
+    // remove dead saxon
+    if (viking.health <= 0) {
+      this.vikingArmy.splice(0, 1);
+    }
+    return result;
+  }
+  showStatus() {}
+}
+
+// vikingAttack() method
+// should be a declared
+// should receive 0 arguments
+// should make Saxon receiveDamage() equal to the strength of a Viking
+// should remove dead saxons from the army
+// should return result of calling receiveDamage() of a Saxon with the strength of a Viking
